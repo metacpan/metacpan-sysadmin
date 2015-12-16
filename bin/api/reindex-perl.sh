adb6620e80f9b264c6781df9113619b1651af419
Olaf Alders <olaf@wundersolutions.com>
2015-12-16 19:23:35 +0000

    Adds script to re-index Perl.

---
+++ bin/api/reindex-perl.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# sh reindex-perl.sh https://cpan.metacpan.org/authors/id/R/RJ/RJBS/perl-5.22.0.tar.bz2 https://cpan.metacpan.org/authors/id/S/SH/SHAY/perl-5.22.1.tar.gz
+
+sudo -u metacpan /home/metacpan/bin/metacpan-api-carton-exec bin/metacpan release $1
+sudo -u metacpan /home/metacpan/bin/metacpan-api-carton-exec bin/metacpan release --status latest $2
