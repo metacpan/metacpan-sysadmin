61ebe9341ce8b615d81c0007ac04a46f3070a7c3
Olaf Alders <olaf@wundersolutions.com>
2017-01-24 12:08:48 -0500

    Force "cpan" status on previous Perl when indexing a new release.

--- bin/api/reindex-perl.sh
+++ bin/api/reindex-perl.sh
@@ -2,5 +2,5 @@
 
 # sh reindex-perl.sh https://cpan.metacpan.org/authors/id/R/RJ/RJBS/perl-5.22.0.tar.bz2 https://cpan.metacpan.org/authors/id/S/SH/SHAY/perl-5.22.1.tar.gz
 
-sudo -u metacpan /home/metacpan/bin/metacpan-api-carton-exec bin/metacpan release $1
+sudo -u metacpan /home/metacpan/bin/metacpan-api-carton-exec bin/metacpan release $1 --status cpan
 sudo -u metacpan /home/metacpan/bin/metacpan-api-carton-exec bin/metacpan release --status latest $2
